ec02c45275c2605aab58ec19c4281b036c592619
saad2999 <79579108+saad2999@users.noreply.github.com>
2022-03-23 22:04:03 +0500

    news app using an api

--- index.js
+++ index.js
@@ -1,86 +1,40 @@
-console.log("index .js added");
-
-function book(name, autor, type) {
-    this.name = name;
-    this.autor = autor;
-    this.type = type;
-
-}
-
-function Display() {
+console.log("index.js in news api")
+let newsAccordion = document.getElementById("newsAccordion")
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://newsapi.org/v2/top-headlines?country=us&q=tech&apiKey=8765897f559040e08de204609c950ae1', true);
 
 
-}
-Display.prototype.add = function(book) {
-        console.log("adding to ui");
-        let tablebody = document.getElementById("tablebody")
 
-        let uistring = ` <tr>
-                   
-                    <td>${book.name}</td>
-                   <td>${book.autor}</td>
-                    <td>@${book.type}</td>
-                    
-                    </tr>`;
-        tablebody.innerHTML += uistring;
-    }
-    // saad
-Display.prototype.clear = function() {
-    let libraryForm = document.getElementById("libraryForm");
 
-    libraryForm.reset();
-}
-Display.prototype.validate = function(book) {
-    if ((book.name.length < 2) || (book.autor.length < 2)) {
-        return false;
+
+xhr.onload = function() {
+    if (this.status === 200) {
+
+        let json = JSON.parse(this.responseText)
+        let artical = json.articles
+        console.log(artical)
+        let newshtml = ""
+        artical.forEach(function(element, index) {
+
+
+            let news = `<div class="accordion-item">
+<h2 class="accordion-header" id="heading${index}">
+    <button class="accordion-button collased" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+  ${element["title"]}
+</button>
+</h2>
+    <div id="collapse${index}" class="accordion-collapse collapse " aria-labelledby="heading${index}" data-bs-parent="newsAccordion">
+            <div class="accordion-body">
+            ${element["content"]}.<a href="${element['url']}" target="_blank rel="">Read more here</a>
+            </div>
+    </div>
+</div>`
+            newshtml += news
+
+        });
+        newsAccordion.innerHTML = newshtml
     } else {
-        return true;
+        console.log("Some error occured")
     }
 }
-Display.prototype.show = function(str, msg) {
-    let myalert = document.getElementById("myalert");
-    let newstr = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>${str}</strong> ${msg}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>`;
-    myalert.innerHTML = newstr;
-    setTimeout(() => {
-        myalert.innerHTML = '';
-    }, 2000);
-
-
-}
-let libraryForm = document.getElementById("libraryForm");
-libraryForm.addEventListener("submit", libraryFormSubmit);
-
-function libraryFormSubmit(e) {
-    e.preventDefault();
-    let name = document.getElementById('bookName').value;
-    let autor = document.getElementById('Author').value;
-    let fiction = document.getElementById("Fiction");
-    let programming = document.getElementById("Programming");
-    let Advanture = document.getElementById("Advanture");
-    let type;
-    if (fiction.checked) {
-        type = fiction.value;
-    } else if (Advanture.checked) {
-        type = Advanture.value;
-
-    } else if (programming.checked) {
-        type = programming.value;
-
-    }
-    let bookobj = new book(name, autor, type);
-    let display = new Display();
-    if (display.validate(bookobj)) {
-        display.add(bookobj);
-        display.clear();
-        display.show("success", "your form is submitted")
-    } else {
-        display.show("error", "you need to fill all fields correctly")
-    }
-
-    console.log("library Form Submitted")
-
-
-}
+xhr.send()
